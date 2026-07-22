7c4d89e3a7cb40ebd6bbff82bc1181b9fc6eb05e
Stanley Huang <stanh2019@gmail.com>
2026-07-21 21:09:29 -0500

    Added rsi function to build_features

--- src/data/features/build_features.py
+++ src/data/features/build_features.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 from pathlib import Path
+import talib
 
 DATA_DIR = Path(__file__).resolve().parents[3] / "data"
 
@@ -26,7 +27,7 @@ def add_log_returns(df: pd.DataFrame, price_col: str = 'close') -> pd.DataFrame:
     )
     return df
 
-# Adds lagged returns to df
+# Adds lagged returns to df with different windows
 def add_lagged_returns(df: pd.DataFrame, lags: list[int] = [1, 5, 10, 20], price_col: str = 'close') -> pd.DataFrame:
     df = df.copy()
     df = df.sort_values(by=['ticker', 'date'])
@@ -39,6 +40,7 @@ def add_lagged_returns(df: pd.DataFrame, lags: list[int] = [1, 5, 10, 20], price
 
     return df
 
+# Adds rolling volatility to df with different windows
 def add_rolling_volatility(df: pd.DataFrame, windows: list[int] = [5, 10, 20], return_col: str = 'log_return') -> pd.DataFrame:
     df = df.copy()
     df = df.sort_values(by=['ticker', 'date'])
@@ -52,16 +54,34 @@ def add_rolling_volatility(df: pd.DataFrame, windows: list[int] = [5, 10, 20], r
     
     return df
 
-# Rolling z score based on close
+# Rolling z score based on close with different windows
 def add_rolling_z_score(df: pd.DataFrame, windows: list[int] = [10, 20, 60], return_col: str = 'close') -> pd.DataFrame:
     df = df.copy()
     df = df.sort_values(by=['ticker', 'date'])
 
     for w in windows:
-        price_col = f'z_{w}d'
+        col_name = f'z_{w}d'
 
-        df[price_col] = df.groupby('ticker')[return_col].transform(
+        df[col_name] = df.groupby('ticker')[return_col].transform(
             lambda x: (x-x.rolling(window=w).mean())/(x.rolling(window=w).std())
         )
     
+    return df
+
+# Adds RSI to df with different windows
+def add_rsi(df: pd.DataFrame, windows: list[int] = [9, 7, 14, 25], return_col: str = 'close') -> pd.DataFrame:
+    delta = df['close'].diff(1)
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    for w in windows:
+        col_name = f'rsi_{w}d'
+        
+        avg_gain = gain.groupby(df['ticker']).transform(lambda x: x.ewm(com=w-1, adjust=False).mean())
+        avg_loss = loss.groupby(df['ticker']).transform(lambda x: x.ewm(com=w-1, adjust=False).mean())
+
+        df[col_name] = df.groupby('ticker')[return_col].transform(
+            lambda x: 100 - (100 / (1 + (avg_gain / avg_loss)))
+        )
+
     return df
